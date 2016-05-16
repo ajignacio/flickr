36242ca9d158bfbf4df0f4b429e99487462946d6
ajignacio <alvinjohn.ignacio@yahoo.com>
2016-05-17 03:38:56 +0800

    remove the comment

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,5 @@
 require File.expand_path('../application', __FILE__)
 require 'flickraw'
 
-#FlickRaw.api_key = ENV['FLICKR_API_KEY']
-#FlickRaw.shared_secret = ENV['FLICKR_SECRET_KEY']
-
 # Initialize the rails application
 FlickrPhoto::Application.initialize!
